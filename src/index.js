656bc15318ec0e58b50dfbbbde702eefea4afd51
ViolaCello <ViolaCelloNYC@gmail.com>
2021-02-10 11:23:10 -0500

    debugged createDeck.  Now, a full deck of 52 cards is created

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 // create a deck of cards from scratch
 
-const ranks = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1"]
+const ranks = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
 const suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
 const createDeck = (ranks, suits) => {
     let fullDeck = []
     ranks.forEach(rank => {
         suits.forEach(suit => {
-            let newCard = {rank:suit}
+            let newCard = {}
+            newCard[rank]=suit
             fullDeck.push(newCard)
         })
     });
