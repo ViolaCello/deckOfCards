83c0a1242058999e948c1ef25a963fcce60bfc77
ViolaCello <ViolaCelloNYC@gmail.com>
2021-02-10 11:36:57 -0500

    #cutTheDeck = checked to see there are at least 52 cards per deck

--- src/index.js
+++ src/index.js
@@ -18,3 +18,19 @@ const createDeck = (ranks, suits) => {
 
 let cards = createDeck(ranks, suits)
 console.log(cards)
+
+// Shuffle the deck
+
+// Step one: Cut the deck
+
+const cutTheDeck = deck => {
+    // check that the deck(s) are a full 52 cards each
+    if (deck.length % 52 != 0) {
+        return ("This is not a full legal deck")
+    } else {
+        console.log (`There is/are ${(deck.length / 52)} full deck(s) here.`)
+    }
+    console.log("we are here now")
+}
+
+cutTheDeck(cards)
