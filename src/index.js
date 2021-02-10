5e11c0d8e524d953e8c1564b9809dde8e9674d1b
ViolaCello <ViolaCelloNYC@gmail.com>
2021-02-10 11:21:05 -0500

    #createDeck

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// create a deck of cards from scratch
+
+const ranks = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1"]
+const suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+const createDeck = (ranks, suits) => {
+    let fullDeck = []
+    ranks.forEach(rank => {
+        suits.forEach(suit => {
+            let newCard = {rank:suit}
+            fullDeck.push(newCard)
+        })
+    });
+    // console.log(fullDeck)
+    return fullDeck
+}
+
+let cards = createDeck(ranks, suits)
+console.log(cards)
